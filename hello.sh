ee9cd586fd331455eb121fee87d5f41aebfbec5c
Soumadeep <deepmicrosoft2022@gmail.com>
2025-10-12 19:30:38 +0530

    1st shell program

---
+++ hello.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/bash
+
+# The above line will tell the interpreter that this is a bash script & it is a mandatory step that we need to write it. The meaning of (#!) is shebang or hashbang then the location of the bash.
+
+echo "hello world" # After echo whatever is written will be printed on the terminal
